af903708004547d1c50333a28945a81cc31793b8
tai160903 <ltt16092003@gmail.com>
2025-06-28 00:46:58 +0700

    add vite cònig

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@services": "/src/services",
+      "@redux": "/src/redux",
+      "@utils": "/src/utils",
+      "@assets": "/src/assets",
+      "@hooks": "/src/hooks",
+      "@styles": "/src/styles",
+    },
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
 });
